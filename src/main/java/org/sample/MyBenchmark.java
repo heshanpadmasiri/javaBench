e7da22d5831c64a49fcc035ba3dc2e27f0c066fc
Heshan Padmasiri <heshanp@wso2.com>
2024-05-09 08:16:22 +0530

    InstanceOf vs tag

--- src/main/java/org/sample/MyBenchmark.java
+++ src/main/java/org/sample/MyBenchmark.java
@@ -31,242 +31,132 @@
 
 package org.sample;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.CompilerControl;
 import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.infra.Blackhole;
 
+import java.util.Random;
+
 public class MyBenchmark {
-    private final static int N = 10_000_000;
-    private static final TableInstruction[] tableInstructions;
-    static {
-        tableInstructions = new TableInstruction[N];
-        for (int i = 0; i < N; i++) {
-            tableInstructions[i] = TableInstruction.getRandom();
-        }
-    }
+    private final static int N = 100_000_000;
+    private final static Insn[] insns;
 
-    private static final DataInstruction[] dataInstruction;
     static {
-        dataInstruction = new DataInstruction[N];
+        insns = new Insn[N];
         for (int i = 0; i < N; i++) {
-            dataInstruction[i] = DataInstruction.getRandom();
+            insns[i] = InstructionBuilder.getRandomInsn();
         }
     }
 
     @Benchmark
     @BenchmarkMode(Mode.AverageTime)
     @Fork(value = 1, warmups = 0)
-    public void opTable(Blackhole blackhole) {
-        for (TableInstruction each: tableInstructions) {
-            TabledOp op = OpsTable.OPS[each.code()];
-            TableOpData res = op.addition(each.v1(), each.v2());
-            blackhole.consume(res);
+    public void tagAndCast(Blackhole blackhole) {
+        for(var insn: insns) {
+            switch (insn.kind()) {
+                case Foo -> {
+                    FooInsn fooInsn = (FooInsn) insn;
+                    blackhole.consume(fooInsn.foo());
+                }
+                case Bar -> {
+                    BarInsn barInsn = (BarInsn) insn;
+                    blackhole.consume(barInsn.bar());
+                }
+                case Baz -> {
+                    BazInsn bazInsn = (BazInsn) insn;
+                    blackhole.consume(bazInsn.baz());
+                }
+                case FooBar -> {
+                    FooBarInsn fooBarInsn = (FooBarInsn) insn;
+                    blackhole.consume(fooBarInsn.fooBar());
+                }
+            }
         }
     }
 
     @Benchmark
     @BenchmarkMode(Mode.AverageTime)
     @Fork(value = 1, warmups = 0)
-    public void virtFn(Blackhole blackhole) {
-        for(var each: dataInstruction) {
-            Data res = each.v1().addition(each.v2());
-            blackhole.consume(res);
-        }
-    }
-}
-
-record DataInstruction(Data v1, Data v2) {
-    static DataInstruction getRandom() {
-        Random rand = new Random();
-        Data v1, v2;
-        switch (rand.nextInt(4)) {
-            case 0: {
-                v1 = new IntData(rand.nextInt());
-                v2 = new IntData(rand.nextInt());
-                break;
-            }
-            case 1: {
-                v1 = new DoubleData(rand.nextDouble());
-                v2 = new DoubleData(rand.nextDouble());
-                break;
-            }
-            case 2: {
-                v1 = new StringData("Hello");
-                v2 = new StringData("World");
-                break;
-            }
-            case 3: {
-                v1 = new FloatData(rand.nextFloat());
-                v2 = new FloatData(rand.nextFloat());
-                break;
-            }
-            default: {
-                throw new RuntimeException("unexpected");
+    public void instanceOfAndCall(Blackhole blackhole) {
+        for(var insn: insns) {
+            if (insn instanceof FooInsn fooInsn) {
+                blackhole.consume(fooInsn.foo());
+            } else if (insn instanceof BarInsn barInsn) {
+                blackhole.consume(barInsn.bar());
+            } else if (insn instanceof BazInsn bazInsn) {
+                blackhole.consume(bazInsn.baz());
+            } else if (insn instanceof FooBarInsn fooBarInsn) {
+                blackhole.consume(fooBarInsn.fooBar());
             }
         }
-        return new DataInstruction(v1, v2);
     }
 }
 
-interface Data {
-    Data addition(Data other);
-}
-
-final class IntData implements Data {
-
-    int value;
-
-    public IntData(int value) {
-        this.value = value;
-    }
-
-    @Override
-    public Data addition(Data other) {
-        IntData otherData = (IntData) other;
-        return new IntData(otherData.value + this.value);
-    }
-}
-final class FloatData implements Data {
-
-    float value;
-
-    public FloatData(float value) {
-        this.value = value;
-    }
-
-    @Override
-    public Data addition(Data other) {
-        FloatData otherData = (FloatData) other;
-        return new FloatData(otherData.value + this.value);
-    }
-}
-final class StringData implements Data {
-
-    String value;
-
-    public StringData(String value) {
-        this.value = value;
-    }
-
-    @Override
-    public Data addition(Data other) {
-        StringData otherData = (StringData) other;
-        return new StringData(otherData.value + this.value);
-    }
-}
-final class DoubleData implements Data {
-
-    double value;
-
-    public DoubleData(double value) {
-        this.value = value;
-    }
-
-    @Override
-    public Data addition(Data other) {
-        DoubleData otherData = (DoubleData) other;
-        return new DoubleData(otherData.value + this.value);
-    }
-}
-record TableInstruction(int code, TableOpData v1, TableOpData v2) {
-    static TableInstruction getRandom() {
-        Random rand = new Random();
-        int op = rand.nextInt(4);
-        TableOpData v1 = switch (op) {
-            case 0 -> new IntegerTableData(rand.nextInt());
-            case 1 -> new FloatTableData(rand.nextFloat());
-            case 2 -> new StringTableData("Hello");
-            case 3 -> new DecimalTableData(rand.nextDouble());
-            default -> throw new IllegalStateException("Unexpected value: " + op);
+final class InstructionBuilder {
+    static Insn getRandomInsn() {
+        return switch (Kind.values()[new Random().nextInt(Kind.values().length)]) {
+            case Foo -> new FooInsn();
+            case Bar -> new BarInsn();
+            case Baz -> new BazInsn();
+            case FooBar -> new FooBarInsn();
+            default -> throw new IllegalStateException();
         };
-        TableOpData v2 = switch (op) {
-            case 0 -> new IntegerTableData(rand.nextInt());
-            case 1 -> new FloatTableData(rand.nextFloat());
-            case 2 -> new StringTableData("World");
-            case 3 -> new DecimalTableData(rand.nextDouble());
-            default -> throw new IllegalStateException("Unexpected value: " + op);
-        };
-        return new TableInstruction(op, v1, v2);
     }
 }
 
-final class OpsTable {
-    static final TabledOp[] OPS;
-
-    static {
-        OPS = new TabledOp[4];
-        OPS[0] = new IntegerTableOp();
-        OPS[1] = new FloatTableOp();
-        OPS[2] = new StringTableOp();
-        OPS[3] = new DecimalTableOp();
-    }
+enum Kind {
+    Foo,
+    Bar,
+    Baz,
+    FooBar,
 }
 
-interface TableOpData {}
-
-interface TabledOp {
-    TableOpData addition(TableOpData v1, TableOpData v2);
+interface Insn {
+    Kind kind();
 }
 
-final class IntegerTableData implements TableOpData {
-    int value;
-    public IntegerTableData(int value) {
-        this.value = value;
-    }
-}
-
-final class FloatTableData implements TableOpData {
-    float value;
-    public FloatTableData(float value) {
-        this.value = value;
-    }
-}
-
-final class StringTableData implements TableOpData {
-    String value;
-    public StringTableData(String value) {
-        this.value = value;
-    }
-}
-
-final class DecimalTableData implements TableOpData {
-    double value;
-    public DecimalTableData(double value) {
-        this.value = value;
-    }
-}
-
-final class IntegerTableOp implements TabledOp {
+class FooInsn implements Insn {
     @Override
-    public TableOpData addition(TableOpData v1, TableOpData v2) {
-        return new IntegerTableData(((IntegerTableData) v1).value + ((IntegerTableData) v2).value);
+    public Kind kind() {
+        return Kind.Foo;
+    }
+
+    int foo() {
+        return 0;
     }
 }
 
-final class FloatTableOp implements TabledOp {
+class BarInsn implements Insn {
     @Override
-    public TableOpData addition(TableOpData v1, TableOpData v2) {
-        return new FloatTableData(((FloatTableData) v1).value + ((FloatTableData) v2).value);
+    public Kind kind() {
+        return Kind.Bar;
+    }
+
+    String bar() {
+        return "";
     }
 }
 
-final class StringTableOp implements TabledOp {
+class BazInsn implements Insn {
     @Override
-    public TableOpData addition(TableOpData v1, TableOpData v2) {
-        return new StringTableData(((StringTableData) v1).value + ((StringTableData) v2).value);
+    public Kind kind() {
+        return Kind.Baz;
+    }
+
+    double baz() {
+        return 0.0;
     }
 }
 
-final class DecimalTableOp implements TabledOp {
+class FooBarInsn implements Insn {
     @Override
-    public TableOpData addition(TableOpData v1, TableOpData v2) {
-        return new DecimalTableData(((DecimalTableData) v1).value + ((DecimalTableData) v2).value);
+    public Kind kind() {
+        return Kind.FooBar;
+    }
+
+    long fooBar() {
+        return 0L;
     }
 }
